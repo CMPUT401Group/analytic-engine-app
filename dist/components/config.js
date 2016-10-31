74dace80d870dd1724f558e40619a6e3beb584a4
Joey Andres <jandres@ualberta.ca>
2016-10-31 13:28:35 -0600

    Analytic engine url in config. Just front end, no saving of config yet.

--- dist/components/config.js
+++ dist/components/config.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
-System.register([], function (_export, _context) {
+System.register(["lodash"], function (_export, _context) {
   "use strict";
 
-  var AnalyticEngineAppConfigCtrl;
+  var _, _createClass, appName, AnalyticEngineAppConfigCtrl;
 
   function _classCallCheck(instance, Constructor) {
     if (!(instance instanceof Constructor)) {
@@ -12,13 +12,82 @@ System.register([], function (_export, _context) {
   }
 
   return {
-    setters: [],
+    setters: [function (_lodash) {
+      _ = _lodash.default;
+    }],
     execute: function () {
-      _export('AnalyticEngineAppConfigCtrl', AnalyticEngineAppConfigCtrl = function AnalyticEngineAppConfigCtrl() {
-        _classCallCheck(this, AnalyticEngineAppConfigCtrl);
-      });
+      _createClass = function () {
+        function defineProperties(target, props) {
+          for (var i = 0; i < props.length; i++) {
+            var descriptor = props[i];
+            descriptor.enumerable = descriptor.enumerable || false;
+            descriptor.configurable = true;
+            if ("value" in descriptor) descriptor.writable = true;
+            Object.defineProperty(target, descriptor.key, descriptor);
+          }
+        }
 
-      _export('AnalyticEngineAppConfigCtrl', AnalyticEngineAppConfigCtrl);
+        return function (Constructor, protoProps, staticProps) {
+          if (protoProps) defineProperties(Constructor.prototype, protoProps);
+          if (staticProps) defineProperties(Constructor, staticProps);
+          return Constructor;
+        };
+      }();
+
+      appName = "Analytic Engine";
+
+      _export("AnalyticEngineAppConfigCtrl", AnalyticEngineAppConfigCtrl = function () {
+        function AnalyticEngineAppConfigCtrl($scope, $injector, $q, backendSrv, alertSrv) {
+          _classCallCheck(this, AnalyticEngineAppConfigCtrl);
+
+          this.$q = $q;
+          this.backendSrv = backendSrv;
+          this.alertSrv = alertSrv;
+          this.appEditCtrl.setPreUpdateHook(this.preUpdate.bind(this));
+          this.appEditCtrl.setPostUpdateHook(this.postUpdate.bind(this));
+
+          // TODO: We will worry about security later. As for the moment,
+          //       anyone can do query to our analyic-engine.
+        }
+
+        _createClass(AnalyticEngineAppConfigCtrl, [{
+          key: "preUpdate",
+          value: function preUpdate() {
+            var model = this.appModel;
+            if (!model.enabled) {
+              return this.$q.resolve();
+            }
+
+            if (!_.isString(model.analyticEngineURL)) {
+              model.enabled = false;
+              this.errorMsg = appName + " Server URL not set";
+              return this.$q.reject(this.errorMsg);
+            }
+
+            return this.$q.resolve();
+          }
+        }, {
+          key: "postUpdate",
+          value: function postUpdate() {
+            if (!this.appModel.enabled) {
+              return this.$q.resolve();
+            }
+
+            return this.$q.then(function () {
+              return self.appEditCtrl.importDashboards().then(function () {
+                return {
+                  url: "dashboard/db/analytic-engine-home",
+                  message: "worldPing app installed!"
+                };
+              });
+            });
+          }
+        }]);
+
+        return AnalyticEngineAppConfigCtrl;
+      }());
+
+      _export("AnalyticEngineAppConfigCtrl", AnalyticEngineAppConfigCtrl);
 
       AnalyticEngineAppConfigCtrl.templateUrl = 'components/config.html';
     }
--- src/components/config.js
+++ src/components/config.js
@@ -1,6 +1,48 @@
+import _ from 'lodash'
+
+let appName = "Analytic Engine";
 
 export class AnalyticEngineAppConfigCtrl {
-  constructor() { }
+  constructor($scope, $injector, $q, backendSrv, alertSrv) {
+    this.$q = $q;
+    this.backendSrv = backendSrv;
+    this.alertSrv = alertSrv;
+    this.appEditCtrl.setPreUpdateHook(this.preUpdate.bind(this));
+    this.appEditCtrl.setPostUpdateHook(this.postUpdate.bind(this));
+
+    // TODO: We will worry about security later. As for the moment,
+    //       anyone can do query to our analyic-engine.
+  }
+
+  preUpdate() {
+    var model = this.appModel;
+    if (!model.enabled) {
+      return this.$q.resolve();
+    }
+
+    if (!_.isString(model.analyticEngineURL)) {
+      model.enabled = false;
+      this.errorMsg = `${appName} Server URL not set`;
+      return this.$q.reject(this.errorMsg);
+    }
+
+    return this.$q.resolve();
+  }
+
+  postUpdate() {
+    if (!this.appModel.enabled) {
+      return this.$q.resolve();
+    }
+
+    return this.$q.then(() => {
+      return self.appEditCtrl.importDashboards().then(() => {
+        return {
+          url: "dashboard/db/analytic-engine-home",
+          message: "worldPing app installed!"
+        };
+      });
+    });
+  }
 }
 AnalyticEngineAppConfigCtrl.templateUrl = 'components/config.html';
 

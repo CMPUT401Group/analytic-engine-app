fa9a845bb5a3e0f29fc57ecd7b525753d133946a
Joey Andres <jandres@ualberta.ca>
2016-11-03 16:20:24 -0600

    debugging metric-entry.

--- dist/panel/module.js
+++ dist/panel/module.js
@@ -52,7 +52,7 @@ System.register(['app/plugins/sdk', '../css/example-app.css!', '../directives/me
         return ExampleAppPanelCtrl;
       }(PanelCtrl));
 
-      ExampleAppPanelCtrl.template = '<h2 class="analytic-engine-app-heading"></h2>' + '<div class="card-section card-list-layout-list" layout-mode>' + '  <ol class="card-list" >' + '    <metric-entry correlation="69" name="metric1"></metric-entry>' + '    <metric-entry correlation="69" name="metric2"></metric-entry>' + '    <metric-entry correlation="69" name="metric3"></metric-entry>' + '    <metric-entry correlation="69" name="metric4"></metric-entry>' + '  </ol>' + '</div>';
+      ExampleAppPanelCtrl.template = '<h2 class="analytic-engine-app-heading"></h2>' + '<div class="card-section card-list-layout-list" layout-mode>' + '  <ol class="card-list" >' + '    <metric-entry name="metric1" correlation="69"></metric-entry>' + '    <metric-entry name="metric2" correlation="69"></metric-entry>' + '    <metric-entry name="metric3" correlation="69"></metric-entry>' + '    <metric-entry name="metric4" correlation="69"></metric-entry>' + '  </ol>' + '</div>';
 
       _export('PanelCtrl', ExampleAppPanelCtrl);
     }
--- src/panel/module.js
+++ src/panel/module.js
@@ -13,10 +13,10 @@ class ExampleAppPanelCtrl extends PanelCtrl {
 ExampleAppPanelCtrl.template = '<h2 class="analytic-engine-app-heading"></h2>' +
   '<div class="card-section card-list-layout-list" layout-mode>' +
   '  <ol class="card-list" >' +
-  '    <metric-entry correlation="69" name="metric1"></metric-entry>' +
-  '    <metric-entry correlation="69" name="metric2"></metric-entry>' +
-  '    <metric-entry correlation="69" name="metric3"></metric-entry>' +
-  '    <metric-entry correlation="69" name="metric4"></metric-entry>' +
+  '    <metric-entry name="metric1" correlation="69"></metric-entry>' +
+  '    <metric-entry name="metric2" correlation="69"></metric-entry>' +
+  '    <metric-entry name="metric3" correlation="69"></metric-entry>' +
+  '    <metric-entry name="metric4" correlation="69"></metric-entry>' +
   '  </ol>' +
   '</div>';
 

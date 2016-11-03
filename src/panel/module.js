5a3e9f0a81f72c26be5ecf3a50311309f5c2801d
Joey Andres <jandres@ualberta.ca>
2016-11-03 16:25:49 -0600

    debugging metric-entry.

--- src/panel/module.js
+++ src/panel/module.js
@@ -13,10 +13,10 @@ class ExampleAppPanelCtrl extends PanelCtrl {
 ExampleAppPanelCtrl.template = '<h2 class="analytic-engine-app-heading"></h2>' +
   '<div class="card-section card-list-layout-list" layout-mode>' +
   '  <ol class="card-list" >' +
-  '    <metric-entry metric-name="metric1" correlation="69"></metric-entry>' +
-  '    <metric-entry metric-name="metric2" correlation="69"></metric-entry>' +
-  '    <metric-entry metric-name="metric3" correlation="69"></metric-entry>' +
-  '    <metric-entry metric-name="metric4" correlation="69"></metric-entry>' +
+  '    <metric-entry data-metric-name="metric1" data-correlation="69"></metric-entry>' +
+  '    <metric-entry data-metric-name="metric2" data-correlation="69"></metric-entry>' +
+  '    <metric-entry data-metric-name="metric3" data-correlation="69"></metric-entry>' +
+  '    <metric-entry data-metric-name="metric4" data-correlation="69"></metric-entry>' +
   '  </ol>' +
   '</div>';
 

21ae483f888c7a14d3b7ba33427c9339c52fb6eb
Joey Andres <jandres@ualberta.ca>
2016-11-03 18:56:24 -0600

    Fix metric-results directive.

--- dist/directives/metric-results.js
+++ dist/directives/metric-results.js
@@ -1,6 +1,6 @@
 'use strict';
 
-System.register(['lodash', 'angular'], function (_export, _context) {
+System.register(['lodash', 'angular', './metric-entry'], function (_export, _context) {
   "use strict";
 
   var _, angular;
@@ -10,7 +10,7 @@ System.register(['lodash', 'angular'], function (_export, _context) {
       _ = _lodash.default;
     }, function (_angular) {
       angular = _angular.default;
-    }],
+    }, function (_metricEntry) {}],
     execute: function () {
 
       angular.module('grafana.directives').directive("metricResults", function ($log) {
@@ -21,7 +21,7 @@ System.register(['lodash', 'angular'], function (_export, _context) {
           },
           replace: true,
           link: function link(scope) {
-            scope.metricResults = [{ name: 'metric-1', value: 23 }, { name: 'metric-2', value: 45 }, { name: 'metric-3', value: 66 }];
+            scope.results = [{ name: 'metric-1', value: 23 }, { name: 'metric-2', value: 45 }, { name: 'metric-3', value: 66 }];
           }
         };
       });
--- src/directives/metric-results.js
+++ src/directives/metric-results.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import angular from 'angular';
 
+import './metric-entry';
+
 angular.module('grafana.directives').directive("metricResults", function($log) {
   return {
     templateUrl: 'public/plugins/analytic-engine-app/directives/metric-results.html',
@@ -9,7 +11,7 @@ angular.module('grafana.directives').directive("metricResults", function($log) {
     },
     replace: true,
     link(scope) {
-      scope.metricResults = [
+      scope.results = [
         { name: 'metric-1', value: 23 },
         { name: 'metric-2', value: 45 },
         { name: 'metric-3', value: 66 }

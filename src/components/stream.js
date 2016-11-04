4a0d93dedc946665aaa1ee3fe388a65d3cac9e78
Sam Bao <sam.b5687@gmail.com>
2016-11-03 21:36:16 -0600

    adding url

--- src/components/stream.js
+++ src/components/stream.js
@@ -4,18 +4,38 @@ import {PanelCtrl} from 'app/plugins/sdk';
 
 export class StreamPageCtrl extends PanelCtrl{
    
-    constructor($scope,$injector){
+    constructor($scope,$injector,backendSrv){
         super($scope,$inejctor);
-        this.pageready = true;
+        this.pageReady = true;
         this.from = Date();
         this.to = Date();
+        this.isOrgAdmin = true;
         this.metricname="";
+        this.results = [];
+        search();
     
     }
 
     search()
     {
 
+        var self = this;
+
+        return this.backendSrv.get('162.246.157.107:8888/pattern/threshold').then(function(resp) {
+
+            if (resp.meta.code !== 200) {
+
+                self.alertSrv.set("failed to get probes.", resp.meta.message, 'error', 10000);
+
+                return self.$q.reject(resp.meta.message);
+
+            }
+
+            self.pageReady = true;
+
+            self.probes = resp.body;
+
+        });
 
     }
 }

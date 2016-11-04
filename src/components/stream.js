3c19b980d6f43d380efb153c8649a1dddc9dad57
Sam Bao <sam.b5687@gmail.com>
2016-11-03 18:28:51 -0600

    adding buttons and search

--- src/components/stream.js
+++ src/components/stream.js
@@ -1,6 +1,19 @@
+import {PaneCtrl} from 'app/plugins/sdk';
 
-export class StreamPageCtrl {
+
+
+export class StreamPageCtrl extends PanelCtrl{
+    this.pageready = true;
+    this.from = Date();
+    this.to = Date();
+    this.metricname="";
+    search()
+    {
+
+
+    }
 }
+
 StreamPageCtrl.templateUrl = 'components/stream.html';
 
 

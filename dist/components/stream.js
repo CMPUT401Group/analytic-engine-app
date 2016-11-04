b5ac9d1bd1bf47ad8bc8d86f1306637b457c7006
Joey Andres <jandres@ualberta.ca>
2016-11-03 21:46:17 -0600

    Debugging $http,

--- dist/components/stream.js
+++ dist/components/stream.js
@@ -1,111 +1,111 @@
 'use strict';
 
 System.register(['app/plugins/sdk'], function (_export, _context) {
-            "use strict";
+    "use strict";
 
-            var PanelCtrl, _createClass, StreamPageCtrl;
+    var PanelCtrl, _createClass, StreamPageCtrl;
 
-            function _classCallCheck(instance, Constructor) {
-                        if (!(instance instanceof Constructor)) {
-                                    throw new TypeError("Cannot call a class as a function");
-                        }
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
+    }
+
+    function _possibleConstructorReturn(self, call) {
+        if (!self) {
+            throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+        }
+
+        return call && (typeof call === "object" || typeof call === "function") ? call : self;
+    }
+
+    function _inherits(subClass, superClass) {
+        if (typeof superClass !== "function" && superClass !== null) {
+            throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
+        }
+
+        subClass.prototype = Object.create(superClass && superClass.prototype, {
+            constructor: {
+                value: subClass,
+                enumerable: false,
+                writable: true,
+                configurable: true
             }
+        });
+        if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
+    }
 
-            function _possibleConstructorReturn(self, call) {
-                        if (!self) {
-                                    throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
-                        }
+    return {
+        setters: [function (_appPluginsSdk) {
+            PanelCtrl = _appPluginsSdk.PanelCtrl;
+        }],
+        execute: function () {
+            _createClass = function () {
+                function defineProperties(target, props) {
+                    for (var i = 0; i < props.length; i++) {
+                        var descriptor = props[i];
+                        descriptor.enumerable = descriptor.enumerable || false;
+                        descriptor.configurable = true;
+                        if ("value" in descriptor) descriptor.writable = true;
+                        Object.defineProperty(target, descriptor.key, descriptor);
+                    }
+                }
 
-                        return call && (typeof call === "object" || typeof call === "function") ? call : self;
-            }
+                return function (Constructor, protoProps, staticProps) {
+                    if (protoProps) defineProperties(Constructor.prototype, protoProps);
+                    if (staticProps) defineProperties(Constructor, staticProps);
+                    return Constructor;
+                };
+            }();
 
-            function _inherits(subClass, superClass) {
-                        if (typeof superClass !== "function" && superClass !== null) {
-                                    throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
-                        }
+            _export('StreamPageCtrl', StreamPageCtrl = function (_PanelCtrl) {
+                _inherits(StreamPageCtrl, _PanelCtrl);
 
-                        subClass.prototype = Object.create(superClass && superClass.prototype, {
-                                    constructor: {
-                                                value: subClass,
-                                                enumerable: false,
-                                                writable: true,
-                                                configurable: true
-                                    }
+                function StreamPageCtrl($scope, $injector, backendSrv) {
+                    _classCallCheck(this, StreamPageCtrl);
+
+                    var _this = _possibleConstructorReturn(this, (StreamPageCtrl.__proto__ || Object.getPrototypeOf(StreamPageCtrl)).call(this, $scope, $inejctor));
+
+                    _this.pageReady = true;
+                    _this.from = Date();
+                    _this.to = Date();
+                    _this.isOrgAdmin = true;
+                    _this.metricname = "";
+                    _this.results = [];
+                    search();
+
+                    return _this;
+                }
+
+                _createClass(StreamPageCtrl, [{
+                    key: 'search',
+                    value: function search() {
+
+                        var self = this;
+
+                        return this.backendSrv.get('162.246.157.107:8888/pattern/threshold').then(function (resp) {
+
+                            if (resp.meta.code !== 200) {
+
+                                self.alertSrv.set("failed to get probes.", resp.meta.message, 'error', 10000);
+
+                                return self.$q.reject(resp.meta.message);
+                            }
+
+                            self.pageReady = true;
+
+                            self.probes = resp.body;
                         });
-                        if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
-            }
+                    }
+                }]);
 
-            return {
-                        setters: [function (_appPluginsSdk) {
-                                    PanelCtrl = _appPluginsSdk.PanelCtrl;
-                        }],
-                        execute: function () {
-                                    _createClass = function () {
-                                                function defineProperties(target, props) {
-                                                            for (var i = 0; i < props.length; i++) {
-                                                                        var descriptor = props[i];
-                                                                        descriptor.enumerable = descriptor.enumerable || false;
-                                                                        descriptor.configurable = true;
-                                                                        if ("value" in descriptor) descriptor.writable = true;
-                                                                        Object.defineProperty(target, descriptor.key, descriptor);
-                                                            }
-                                                }
+                return StreamPageCtrl;
+            }(PanelCtrl));
 
-                                                return function (Constructor, protoProps, staticProps) {
-                                                            if (protoProps) defineProperties(Constructor.prototype, protoProps);
-                                                            if (staticProps) defineProperties(Constructor, staticProps);
-                                                            return Constructor;
-                                                };
-                                    }();
+            _export('StreamPageCtrl', StreamPageCtrl);
 
-                                    _export('StreamPageCtrl', StreamPageCtrl = function (_PanelCtrl) {
-                                                _inherits(StreamPageCtrl, _PanelCtrl);
-
-                                                function StreamPageCtrl($scope, $injector, backendSrv) {
-                                                            _classCallCheck(this, StreamPageCtrl);
-
-                                                            var _this = _possibleConstructorReturn(this, (StreamPageCtrl.__proto__ || Object.getPrototypeOf(StreamPageCtrl)).call(this, $scope, $inejctor));
-
-                                                            _this.pageReady = true;
-                                                            _this.from = Date();
-                                                            _this.to = Date();
-                                                            _this.isOrgAdmin = true;
-                                                            _this.metricname = "";
-                                                            _this.results = [];
-                                                            search();
-
-                                                            return _this;
-                                                }
-
-                                                _createClass(StreamPageCtrl, [{
-                                                            key: 'search',
-                                                            value: function search() {
-
-                                                                        var self = this;
-
-                                                                        return this.backendSrv.get('162.246.157.107:8888/pattern/threshold').then(function (resp) {
-
-                                                                                    if (resp.meta.code !== 200) {
-
-                                                                                                self.alertSrv.set("failed to get probes.", resp.meta.message, 'error', 10000);
-
-                                                                                                return self.$q.reject(resp.meta.message);
-                                                                                    }
-
-                                                                                    self.pageReady = true;
-
-                                                                                    self.probes = resp.body;
-                                                                        });
-                                                            }
-                                                }]);
-
-                                                return StreamPageCtrl;
-                                    }(PanelCtrl));
-
-                                    _export('StreamPageCtrl', StreamPageCtrl);
-
-                                    StreamPageCtrl.templateUrl = 'components/stream.html';
-                        }
-            };
+            StreamPageCtrl.templateUrl = 'components/stream.html';
+        }
+    };
 });
 //# sourceMappingURL=stream.js.map
